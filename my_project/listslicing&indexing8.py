0ded1dd537970ae4b49f4be8381ac5fb84d289af
k7-1304 <k71304@gmail.com>
2025-09-08 17:31:07 +0530

    Add files via upload

---
+++ my_project/listslicing&indexing8.py
@@ -0,0 +1,6 @@
+prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+print("middle five prime numbers:", prime_numbers[3:8])
+print("Every second prime number:", prime_numbers[::2])
+print("Reversed list of prime numbers:", prime_numbers[::-1])
+print("Last three prime numbers:", prime_numbers[-3:])
