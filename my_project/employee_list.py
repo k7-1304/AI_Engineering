5677683e4d559398ec4e6e5658947c0aef7121a6
k7-1304 <k71304@gmail.com>
2025-09-11 07:30:03 +0530

    Delete my_project/employee_list.py

--- my_project/employee_list.py
+++
@@ -1,6 +0,0 @@
-# Step 1: Create a list of 5 employee names
-employees = ["Alice", "Kesav", "Charlie", "David", "Jyothi"]
-
-# Step 2: Use a for loop with enumerate to print numbered names
-for i, n in enumerate(employees, start=1):
-    print(f"{i}. {n}")
